59b5c424995daf521c90d64952141faa36c5d11b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-16 23:32:56 +0500

    Displays ceiling number

--- Free_Code_Camp/numbers.c
+++ Free_Code_Camp/numbers.c
@@ -52,6 +52,9 @@ int main()
     // Prints square root number.
     printf("Square Root Number: %f\n", sqrt(36));
 
+    // Prints ceiling number (next highest number).
+    printf("Ceiling Number: %f\n", ceil(36.356));
+
     return 0;
 }
 
