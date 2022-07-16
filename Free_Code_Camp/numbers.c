a9aa4e27091c6684e58f9f1d77e9e291ca656e49
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-16 22:49:29 +0500

    Prints basic integers

--- Free_Code_Camp/numbers.c
+++ Free_Code_Camp/numbers.c
@@ -21,13 +21,20 @@ Note: Uncomment codes to execute and comment them when not in use.
 int main()
 {
     // Prints numbers (Floating points).
-    printf("Number: %f\n\n", 6.7);
+    printf("Floating Point Number: %f\n\n", 6.7);
+
+    // Printing basic arithmetic operations.
+    printf("------BASIC INTEGERS------\n");
+    printf("Addition Total: %d\n", 3 + 2); // Addition.
+    printf("Subtraction Total: %d\n", 5 - 3); // Subtraction.
+    printf("Multiplication Total: %d\n" , 23 * 1); // Multiplication.
+    printf("Division Total: %d\n\n", 20 / 2); // Division.
 
     // Printing basic arithmetic operations (Floating Points).
-    printf("------FLOATING POINTS------");
+    printf("------FLOATING POINTS------\n");
     printf("Addition Total: %f\n", 3.0 + 2.0); // Addition.
     printf("Subtraction Total: %f\n", 5.0 - 3.0); // Subtraction.
-    printf("Multiplies Total: %f\n", 23.0 * 1.0); // Multiplication.
+    printf("Multiplication Total: %f\n", 23.0 * 1.0); // Multiplication.
     printf("Division Total: %f\n", 20.0 / 2.0); // Division.
 
     return 0;
