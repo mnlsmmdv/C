3ed30e20f75ef79b92aa144998c065e5e81bbbfc
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-17 23:59:13 +0500

    New file created

---
+++ Free_Code_Camp/constants.c
@@ -0,0 +1,28 @@
+/*
+Name: Ahmed Affaan
+Title: constants.c
+Folder: C/Free_Code_Camp
+Date: 17/07/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: Giraffe Academy
+Credits to link: https://www.youtube.com/watch?v=KJgsSFOSQv0
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+// Importing libraries.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Main function.
+int main()
+{
+    // Printing a message.
+    printf("Hello World!\n");
+    return 0;
+}
+
+// Program end.
