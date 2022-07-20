c17338cdb44c11fe35b15f4a2407afcbff9f2295
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-20 20:15:49 +0500

    File correctly named

---
+++ Free_Code_Camp/DataTypes.c
@@ -0,0 +1,32 @@
+/*
+Name: Ahmed Affaan
+Title: data_types.c
+Folder: C/Free_Code_Camp
+Date: 09/07/2022
+Country: Republic of Maldives
+Code version: -
+Description: -
+Credits to: Giraffe Academy
+Credits to link: https://www.youtube.com/watch?v=KJgsSFOSQv0
+Note: Uncomment codes to execute and comment them when not in use.
+*/
+
+// Program start.
+
+// Importing libraries.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Main function.
+int main()
+{
+    // Basic data types used in the C programming language.
+    int age = 40; // Integer.
+    double gpa = 3.7 // Decimal number.
+    char grade = 'A'; // Single characters.
+    char phrase[] = "Giraffe Academy"; // Strings.
+
+    return 0;
+}
+
+// Program end.
