6e50982f946a80f9ba0d576e7ae59c2ff04e2c4b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-07-23 15:38:12 +0500

    Formatting

--- Free_Code_Camp/Structs.c
+++ Free_Code_Camp/Structs.c
@@ -49,7 +49,6 @@ int main(){
     student3.studentAge = 24;
     student3.studentGpa = 3.6;
 
-
     printf("+-------------------------------+\n"); // Separator.
     printf("|         STUDENT DETAILS       |\n");
     // Printing Student 1 details.
